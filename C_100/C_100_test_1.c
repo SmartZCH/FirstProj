7cdffc9b1259cd8275d800dc885c97b7cb250109
Changhao Zhu 朱长昊 <changhao.zhu@MBC02F4154MD6M.local>
2021-04-14 17:49:35 +0800

    change

---
+++ C_100/C_100_test_1.c
@@ -0,0 +1,25 @@
+//题目：有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
+#include <stdio.h>
+int main(){
+    int i,j,k,num=0;
+    for(i=1;i<5;i++)
+    {
+        for(j=1;j<5;j++)
+        {
+            for(k=1;k<5;k++)
+            {
+                if (i!=j&&j!=k&&i!=k)
+                {
+                    printf("%d%d%d\n",i,j,k);//打印出可能存在的所有的数字
+                    num++;
+                }
+            }
+        }
+    }
+printf("%d",num++);
+return 0;
+}
+
+
+
+
