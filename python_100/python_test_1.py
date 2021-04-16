fe003464faf28d14c2787df637fbd33da3cdd345
Changhao Zhu 朱长昊 <changhao.zhu@MBC02F4154MD6M.local>
2021-04-16 10:38:12 +0800

    change

--- python_100/python_test_1.py
+++
@@ -1,7 +0,0 @@
-'''题目： 有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？'''
-
-'''for i in range(1,4)
-    for j in range(1,4)
-        for k in range（1，4）
-            if （i!=j）&(i!=k) &（j!=k）'''
-print ("hello")
