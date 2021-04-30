e45f80e84c3a283f37aa9e3690fb6b8577a4a1eb
Changhao Zhu 朱长昊 <changhao.zhu@MBC02F4154MD6M.local>
2021-04-30 16:47:15 +0800

    change

--- C_nvos_1/C_nvos_1.c
+++ C_nvos_1/C_nvos_1.c
@@ -15,59 +15,43 @@
 
 char * addBinary(char * a, char * b)
 {
-	// 进位
-    int carry = 0;
-    // 记录开辟空间大小
-    // 需要预留最高位进位、'\0'的空间
-	int length = (strlen(a)>strlen(b)? strlen(a)+2:strlen(b)+2);
-    // 开辟空间
-	char* result = (char*)malloc(sizeof(char)*length);
-    // 设置'\0'
-	result[length-1] = '\0';
-    // i记录a的最后一位
-    // j记录b的最后一位
-    // k记录result[]的最后一位，倒着存放求和结果
-	for(int i=strlen(a)-1 , j=strlen(b)-1 , k=length-2 ; (i>=0)||(j>= 0) ; i--,j--,k--)
-	{
-        // sum记录当前位数，先加上前一位的进位1或者0
-		int sum = carry;
-        // 位数不匹配时，要给空位赋0值，才能继续按位相加
-		sum += (i >= 0? a[i]-'0':0);
-		sum += (j >= 0? b[j]-'0':0);
-
-
-        // 更新进位值
-        // 方法一：
-        if(sum>1)
-        {
-            carry=1;
-            sum-=2;
+	int len_a=strlen(a);//a的长度
+    int len_b=strlen(b);//b的长度
+    int m=(strlen(a)>strlen(b)?strlen(a):strlen(b));//m放最大的长度
+    char *n=(char *)malloc(sizeof(char)*m+2);//生成n,有最高位还会逢二进一的情况,还有尾部的'\0',所以大小为最大数组的长度再加二
+    n[m+1]='\0';
+    for(int i=0;i<m+1;i++)
+    {
+        n[i]='0';//新数组初始化
+    }
+    int t=strlen(n),t1=strlen(n),t2=strlen(n);
+    while(len_a!=0 || len_b!=0){
+        if(len_a){
+            n[--t1]+=a[--len_a]-48;//n初始化时便为'0',所以减去多余的'0'再加
         }
-        else
-        {
-            carry=0;
+        if(len_b){
+            n[--t2]+=b[--len_b]-48;
         }
-        result[k]=sum+'0';
-        // 方法二：对应位求和，然后求商作进位位，剩下的余数为结果位。
-		//carry = sum /2;
-		//result[k] = '0'+ sum % 2;
-	}
-	// 最高位无进位，直接返回
-	if(carry == 0)
-        // 此时result[0]为空，故指针+1
-		return result+1;
-    //有进位，补一个最高位
-	result[0] = '1';
-		return result;
+        t--;    
+        if(n[t]=='2'){
+            n[t-1]++;//进一位
+            n[t]='0';
+        }
+        if(n[t]=='3'){
+            n[t-1]++;
+            n[t] = '1';
+        }
+    }
+    return n;
 }
 int main(void)
 {
-    char * a = "1011";
-    char * b = "0111"; 
+    char * a = "1010";
+    char * b = "1011"; 
     char *ret = addBinary(a, b);
     if (ret)
     {
-        printf("0b%s + 0b%s = 0b%s\n", a, b, ret);
+        printf("0b%s + 0b%s = 0b%s", a, b, ret);
         free(ret);
     }
 }
